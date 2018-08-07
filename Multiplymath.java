b7da325d630156151c49481ccbe659362f32d365
rashid ahmad <rashidahmad03@gmail.com>
2018-08-07 23:08:21 +0530

    Add files via upload

--- Multiplymath.java
+++ Multiplymath.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Multiplymath {
     public static void main(String[] args) throws IOException {
@@ -17,13 +18,15 @@ System.out.println("               ");
             mul = mul + str.charAt(i);
         }
         int a[]=new int[str.length()];
-
+        int b[]=new int[str.length()];
         for(int i=0;i<str.length();i++)
         {
            int  d=multi(Integer.parseInt(String.valueOf(mul.charAt(i))),s);
+           b[i]=d;
             a[i]= d*(int)(Math.pow(10,i));
-            System.out.println(a[i]);
+
         }
+        printpatter(b);
 System.out.println("..................................");
       int sum=0;
         for(int i=0;i<a.length;i++)
@@ -40,7 +43,18 @@ return c*s;
 
 
     }
-static void  printpatter(){
+static void  printpatter(int[] a){
+        for(int i=0;i<a.length;i++)
+        {
+            for(int j=i;j<a.length;j++)
+            {
+                System.out.print(" ");
+            }
+
+                System.out.print(a[i]);
+
+            System.out.println();
+        }
 
 }
 
